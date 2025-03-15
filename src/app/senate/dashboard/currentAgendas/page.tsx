d8e2690153698538b417c38869809058b1afc91d
Nahome18 <101459741+Nahome18@users.noreply.github.com>
2025-03-14 21:28:20 -0500

    senate role buttons

--- src/app/senate/dashboard/currentAgendas/page.tsx
+++ src/app/senate/dashboard/currentAgendas/page.tsx
@@ -1,18 +1,34 @@
 'use client';
+import { useState, useEffect } from "react";
 import styles from './currentAgendas.module.css'
 import { useCollapsedContext } from '../../../components/sideBar/sideBarContext'
 import SideBar from '../../../components/sideBar/SideBar'
 import AgendaSection from '../../../components/agendaSection/agendaSection'
 import AgendaData from '../../../agendas.json'
 import AddAgenda from '../../../components/addAgenda/addAgenda'
+import { useUser } from "@clerk/nextjs";
 
 export default function CurrentAgendas(){
     const { collapsed, setCollapsed } = useCollapsedContext();
+    const { user, isSignedIn } = useUser();
+    const [isMember, setIsMember] = useState<boolean>(false);
+    const [isSpeaker, setIsSpeaker] = useState<boolean>(true);
+
+    // useEffect(() => {
+    //     if (isSignedIn && user?.publicMetadata?.role === "senate_member") {
+    //         setIsMember(true);
+    //         console.log(user.publicMetadata.role)
+    //       }
+    //     else if (isSignedIn && user?.publicMetadata?.role === "senate_speaker") {
+    //         setIsSpeaker(true);
+    //       }
+    //   }, [user]);
+
     return (
         <div className={styles.currentAgendas}>
             <div className={styles.top}>
                 <h1>Current Agendas</h1>
-                <AddAgenda />
+                {isSpeaker && <AddAgenda />}
             </div>
 
             <SideBar collapsed={collapsed} setCollapsed={setCollapsed}/>
@@ -21,14 +37,14 @@ export default function CurrentAgendas(){
                     <div className={styles.labels}>
                         <label>Title</label>
                         <div className={styles.rightLabels}>
-                            <label>Voted</label>
-                            <label>Visible</label>
+                            {isMember && <label>Voted</label>}
+                            {isSpeaker && <label>Visible</label>}
                         </div>
                     </div>
                 {AgendaData.map((item, index) => (
                     !item.closed && 
                     (
-                    <AgendaSection key={index} agenda={item} page={'current'}/>
+                    <AgendaSection key={index} agenda={item} page={'current'} isMember={isMember} isSpeaker={isSpeaker}/>
                     )
                 ))}
                 </div>
